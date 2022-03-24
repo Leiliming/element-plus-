1a1e2bba63c7abae5795d6cfaab3290c6ea575c1
力铭 <38111405+Leiliming@users.noreply.github.com>
2022-03-24 16:22:49 +0800

    把icon驼峰转换成横杠连接

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import router from './router/index'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as Icons from '@element-plus/icons-vue'
+import { toLine } from './utils'
 const app = createApp(App)
 //全局注册图标 牺牲一点性能
 //el-icon-xxx
 for (let i in Icons) {
-  console.log((Icons as any)[i])
+  //注册全部组件
+  app.component(`el-icon${toLine(i)}`, (Icons as any)[i])
 }
 app.use(router).use(ElementPlus)
 app.mount('#app')
